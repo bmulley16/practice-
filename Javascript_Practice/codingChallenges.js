e6ad3e8b24d7ad8a6a0f03bae961afbe2737fe28
Bryce Mulley <bmulley9816@gmail.com>
2023-01-15 23:39:53 -0330

    Added some JavaScript Practice from udemy coding challenges

---
+++ Javascript_Practice/codingChallenges.js
@@ -0,0 +1,45 @@
+let markWeight = 78;
+let markHeigh = 1.69;
+
+let johnWeight = 92;
+let johnHeight = 1.95;
+
+markBMI = markWeight / (markHeigh ** 2);
+console.log(markBMI)
+
+johnBMI = johnWeight / (johnHeight ** 2);
+console.log(johnBMI)
+
+markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI)
+
+if (markHigherBMI) {
+    console.log(`Marks BMI is higher than John's`)
+} else {
+    console.log(`John's BMI is higher than Marks`)
+}
+
+if (markHigherBMI) {
+    console.log(`Marks BMI (${markBMI}) is higher than John's BMI (${johnBMI})`)
+}
+else {
+    console.log(`John's BMI (${johnBMI}) is higher than Marks BMI (${markBMI})`)
+}
+
+const dolphinsAvgScore = (96 + 108 + 89) / 3;
+console.log(dolphinsAvgScore)
+const koalasAvgScore = (88 + 91 + 110) / 3;
+console.log(koalasAvgScore)
+
+if (dolphinsAvgScore === koalasAvgScore && dolphinsAvgScore >= 100 && koalasAvgScore >= 100) {
+    console.log("It is a draw!")
+}
+else if (dolphinsAvgScore > koalasAvgScore && dolphinsAvgScore >= 100) {
+    console.log(`Dolphins win the Prize`)
+}
+else if (koalasAvgScore > dolphinsAvgScore && koalasAvgScore >= 100) {
+    console.log(`Koalas Win the price`)
+}
+else {
+    console.log(`Nobody wins the trophy`)
+}
